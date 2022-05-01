aea03cd657f8e7f72e158d75c789da590344c5e3
sfm999 <sfm999@uowmail.edu.au>
2022-05-01 19:20:35 +1000

    temp test idk

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -3,29 +3,35 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
-const Item = styled(Paper({elevation:0}))(({ theme }) => ({
+const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    boxShadow: 'none',
+    maxHeight: "80px",
 
   }));
 
 export default function Plan({plan}){
     return(
         <Grid>
-            <Typography variant='h5'gutterBottom component='div'>
-                Payment Plan
-            </Typography>
+            <Item>
+                <Typography variant='h5' gutterBottom component='div'>
+                    Payment Plan
+                </Typography>
+            </Item>
 
            <Item> 
                 <h3>
                     {plan.type}
                 </h3>                
             </Item>
-
-            <Button variant ='outlined' size="small" >Change Payment Plan</Button>
+            <Item>
+                <Button variant ='outlined'
+                    sx={{minWidth: "100%"}}>Change Payment Plan</Button>
+            </Item>
         </Grid>
     );
 }
