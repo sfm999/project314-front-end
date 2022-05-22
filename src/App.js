cb6dc1aa47029faf9af5f63a15ab129b4faeda77
Kaine <kainevangemert@gmail.com>
2022-05-22 16:53:13 +1000

    Updated router and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import './App.css';
-import CustomerProfile from './components/Customer_Pages/CustomerProfile';
-import SignIn from './components/Customer_Pages/SignIn';
-import SignUp from './components/Customer_Pages/SignUp';
-import ContractorSignUp from './components/Contractor_Pages/ContractorSignUp';
-import ContractorSignIn from './components/Contractor_Pages/ContractorSignIn';
-import ContractorProfile from './components/Contractor_Pages/ContractorProfile';
-import ContractorHome from './components/Contractor_Pages/ContractorHome';
-import CustomerHomePage from './components/Customer_Pages/CustomerHomePage';
-import TestPage from './components/TestPage';
-import ManageVehicle from './components/Customer_Pages/ManageVehicle';
-import AddVehicle from './components/Customer_Pages/AddVehicle';
-import EditVehicle from './components/Customer_Pages/EditVehicle';
-import PaymentProcess from './components/Customer_Pages/PaymentProcess';
-
-
-
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/Home";
+import "./App.css";
+import CustomerProfile from "./components/Customer_Pages/CustomerProfile";
+import SignIn from "./components/Customer_Pages/SignIn";
+import SignUp from "./components/Customer_Pages/SignUp";
+import ContractorSignUp from "./components/Contractor_Pages/ContractorSignUp";
+import ContractorSignIn from "./components/Contractor_Pages/ContractorSignIn";
+import ContractorProfile from "./components/Contractor_Pages/ContractorProfile";
+import ContractorHome from "./components/Contractor_Pages/ContractorHome";
+import CustomerHomePage from "./components/Customer_Pages/CustomerHomePage";
+import TestPage from "./components/TestPage";
+import ManageVehicle from "./components/Customer_Pages/ManageVehicle";
+import AddVehicle from "./components/Customer_Pages/AddVehicle";
+import EditVehicle from "./components/Customer_Pages/EditVehicle";
+import PaymentProcess from "./components/Customer_Pages/PaymentProcess";
+import useAuth from "./hooks/useAuth";
 
 function App() {
+  const { isAuthenticated } = useAuth();
+  console.log(isAuthenticated);
   return (
     <Router>
       <div className="App">
@@ -29,40 +34,61 @@ function App() {
           {/* The pages we want the router to keep track of */}
           <Routes>
             {/* Generic Home Page */}
-            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/"
+              element={
+                isAuthenticated ? (
+                  <Navigate to="/customer/home" />
+                ) : (
+                  <Navigate to="/customer/sign-in" />
+                )
+              }
+            />
+
+            <Route
+              exact
+              path="/customer"
+              element={
+                isAuthenticated ? (
+                  <Navigate to="/customer/home" />
+                ) : (
+                  <Navigate to="/customer/sign-in" />
+                )
+              }
+            />
 
             {/* Custome Home Page */}
-            <Route path="/CustomerHomePage" element={<CustomerHomePage />} />
+            <Route path="/customer/home" element={<CustomerHomePage />} />
 
             {/* Customer(?) sign in and sign up */}
-            <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/customer/sign-in" element={<SignIn />} />
+            <Route path="/customer/sign-up" element={<SignUp />} />
 
             {/* Contractor sign in and sign up */}
-            <Route path="/ContractorSignUp" element={<ContractorSignUp />} />
-            <Route path="/ContractorSignIn" element={<ContractorSignIn />} />
+            <Route path="/contractor/sign-up" element={<ContractorSignUp />} />
+            <Route path="/contractor/sign-in" element={<ContractorSignIn />} />
 
             {/* Contractor Home Page and Profile */}
-            <Route path="/ContractorHome" element={<ContractorHome />} />
-            <Route path="/ContractorProfile" element={<ContractorProfile />} />
+            <Route path="/contractor/home" element={<ContractorHome />} />
+            <Route path="/contractor/profile" element={<ContractorProfile />} />
 
             {/* Customer Profile Page */}
-            <Route path="/CustomerProfile" element={<CustomerProfile />} />
+            <Route path="/customer/profile" element={<CustomerProfile />} />
 
-          {/* Customer Profile Page */}
-          <Route path="/CustomerProfile" element={<CustomerProfile />} />
+            {/* Pages for Customer Functionality */}
+            <Route
+              path="/customer/vehicles/manage"
+              element={<ManageVehicle />}
+            />
+            <Route path="/customer/vehicles/add" element={<AddVehicle />} />
+            <Route path="/customer/vehicles/edit" element={<EditVehicle />} />
+            <Route path="/customer/payment" element={<PaymentProcess />} />
 
-          {/* Pages for Customer Functionality */}
-          <Route path="/manageVehicles" element={<ManageVehicle />} />
-          <Route path="/addVehicle" element={<AddVehicle />} />
-          <Route path="/editVehicle" element={<EditVehicle />} />
-          <Route path="/paymentProcess" element={<PaymentProcess />} />
-
-          {/* Just a page to test things out on */}
-          <Route path="/testpage" element={<TestPage />} />
-
-        </Routes>
-        {/* END ROUTES */}
+            {/* Just a page to test things out on */}
+            <Route path="/customer/test-page" element={<TestPage />} />
+          </Routes>
+          {/* END ROUTES */}
         </div>
       </div>
     </Router>
