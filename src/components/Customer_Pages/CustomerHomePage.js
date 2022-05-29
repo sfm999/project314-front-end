ff1a218c41154d68d1d2a13f27b53cbaaee12d11
Kaine <kainevangemert@gmail.com>
2022-05-29 14:35:14 +1000

    fix needing vehicle to display home page

--- src/components/Customer_Pages/CustomerHomePage.js
+++ src/components/Customer_Pages/CustomerHomePage.js
@@ -330,9 +330,9 @@ const CustomerHomePage = () => {
                   <ListItemText
                     primary="Vehicle List"
                     secondary={
-                      vehicleList[selectedIndex].make +
+                      vehicleList[selectedIndex]?.make +
                       " " +
-                      vehicleList[selectedIndex].model
+                      vehicleList[selectedIndex]?.model
                     }
                   />
                 </ListItem>
